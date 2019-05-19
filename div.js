f2698a5c64807420028844040d08099f171d42c6
jntsimplu <36079630+jntsimplu@users.noreply.github.com>
2019-05-19 03:22:09 +0300

    updates

--- div.js
+++ div.js
@@ -1,52 +1,52 @@
-for (var i=1;i<=11;i++){
-    document.write("<div class=\"slot\" id=\"slot-"+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
-    var pixel = String(90+((i-1)*45))+'px';
-    document.getElementById('slot-'+i).style.top = pixel;
+for (var i=1;i<=9;i++){
+    document.write("<div class=\"slot\" id=\""+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
+    var pixel = String(140+((i-1)*45))+'px';
+    document.getElementById(i).style.top = pixel;
 }
-for (var i=12;i<=23;i++){
-    document.write("<div class=\"slot\" id=\"slot-"+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
-    var pixel = String(225+((i-12)*60))+'px';
-    document.getElementById('slot-'+i).style.left = pixel;
+for (var i=10;i<=20;i++){
+    document.write("<div class=\"slot\" id=\""+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
+    var pixel = String(225+((i-10)*60))+'px';
+    document.getElementById(i).style.left = pixel;
 }
-for (var i=24;i<=34;i++){
-    document.write("<div class=\"slot\" id=\"slot-"+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
-    var leftPixel = String(225+((i-24)*60))+'px';
-    document.getElementById('slot-'+i).style.left = leftPixel;
-    var topPixel = String(180)+'px';
-    document.getElementById('slot-'+i).style.top = topPixel;
+for (var i=21;i<=30;i++){
+    document.write("<div class=\"slot\" id=\""+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
+    var leftPixel = String(225+((i-21)*60))+'px';
+    document.getElementById(i).style.left = leftPixel;
+    var topPixel = String(185)+'px';
+    document.getElementById(i).style.top = topPixel;
 }
-for (var i=35;i<=45;i++){
-    document.write("<div class=\"slot\" id=\"slot-"+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
-    var leftPixel = String(225+((i-35)*60))+'px';
-    document.getElementById('slot-'+i).style.left = leftPixel;
-    var topPixel = String(180+45+45)+'px';
-    document.getElementById('slot-'+i).style.top = topPixel;
+for (var i=31;i<=40;i++){
+    document.write("<div class=\"slot\" id=\""+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
+    var leftPixel = String(225+((i-31)*60))+'px';
+    document.getElementById(i).style.left = leftPixel;
+    var topPixel = String(185+45+45)+'px';
+    document.getElementById(i).style.top = topPixel;
 }
-for (var i=46;i<=56;i++){
-    document.write("<div class=\"slot\" id=\"slot-"+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
-    var leftPixel = String(225+((i-46)*60))+'px';
-    document.getElementById('slot-'+i).style.left = leftPixel;
-    var topPixel = String(180+45+45+45+45)+'px';
-    document.getElementById('slot-'+i).style.top = topPixel;
+for (var i=41;i<=50;i++){
+    document.write("<div class=\"slot\" id=\""+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
+    var leftPixel = String(225+((i-41)*60))+'px';
+    document.getElementById(i).style.left = leftPixel;
+    var topPixel = String(185+45+45+45+45)+'px';
+    document.getElementById(i).style.top = topPixel;
 }
-for (var i=57;i<=67;i++){
-    document.write("<div class=\"slot\" id=\"slot-"+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
-    var leftPixel = String(225+((i-57)*60))+'px';
-    document.getElementById('slot-'+i).style.left = leftPixel;
-    var topPixel = String(180+45+45+45+45+45+45)+'px';
-    document.getElementById('slot-'+i).style.top = topPixel;
+for (var i=51;i<=60;i++){
+    document.write("<div class=\"slot\" id=\""+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
+    var leftPixel = String(225+((i-51)*60))+'px';
+    document.getElementById(i).style.left = leftPixel;
+    var topPixel = String(185+45+45+45+45+45+45)+'px';
+    document.getElementById(i).style.top = topPixel;
 }
-for (var i=68;i<=79;i++){
-    document.write("<div class=\"slot\" id=\"slot-"+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
-    var leftPixel = String(165+((i-68)*60))+'px';
-    document.getElementById('slot-'+i).style.left = leftPixel;
+for (var i=61;i<=71;i++){
+    document.write("<div class=\"slot\" id=\""+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
+    var leftPixel = String(165+((i-61)*60))+'px';
+    document.getElementById(i).style.left = leftPixel;
     var topPixel = String(180+45+45+45+45+45+45+45+45)+'px';
-    document.getElementById('slot-'+i).style.top = topPixel;
+    document.getElementById(i).style.top = topPixel;
 }
-for (var i=80;i<=90;i++){
-    document.write("<div class=\"slot\" id=\"slot-"+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
-    var leftPixel = String(945)+'px';
-    document.getElementById('slot-'+i).style.left = leftPixel;
-    var topPixel = String(90+((i-80)*45))+'px';
-    document.getElementById('slot-'+i).style.top = topPixel;
+for (var i=72;i<=80;i++){
+    document.write("<div class=\"slot\" id=\""+i+"\" onclick=\"slotSelected()\">"+i+"</div>");
+    var leftPixel = String(885)+'px';
+    document.getElementById(i).style.left = leftPixel;
+    var topPixel = String(140+((i-72)*45))+'px';
+    document.getElementById(i).style.top = topPixel;
 }
