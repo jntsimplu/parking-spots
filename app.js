6eb1f3904f8ee73e76232d29d618df8131211cb2
jntsimplu <36079630+jntsimplu@users.noreply.github.com>
2019-05-19 03:21:44 +0300

    updates

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
 var oneSelected=0;
 var parkingLots=[];
-parkingLots.fill(0,0,89);
-console.log(parkingLots);
+var numberOfLots=80;
+for (var i=0;i<80;i++) parkingLots[i]=0;
 function slotSelected(){
     /*variables*/
     selectedId=event.srcElement.id;
-    selectedColor=document.getElementById(selectedId).style.backgroundColor;
     if (oneSelected===0){
         document.getElementById(selectedId).style.backgroundColor='orange';
         oneSelected=1;
         document.querySelector('.btn-buy').addEventListener('click',function(){
-            document.getElementById(selectedId).style.backgroundColor='red';
-            oneSelected=0;
-            selectedId='';
+            if (parkingLots[selectedId-1]===0){
+                document.getElementById(selectedId).style.backgroundColor='red';
+                parkingLots[selectedId-1]=1;
+                oneSelected=0;
+                selectedId='';
+                numberOfLots--;
+                document.getElementById('clicks').textContent=numberOfLots;
+            } else {
+                document.getElementById(selectedId).style.backgroundColor='red';
+                alert('You cant buy a lot twice!');
+                oneSelected=0;
+                selectedId='';
+            }
         });
         document.querySelector('.btn-rej').addEventListener('click',function(){
-            document.getElementById(selectedId).style.backgroundColor='rgb(66, 134, 49)';
-            oneSelected=0;
-            selectedId='';
-        });
-    }
-    if (oneSelected===1&&selectedColor==='red'){
-        document.getElementById(selectedId).style.backgroundColor='blue';
-        document.querySelector('.btn-buy').addEventListener('click',function(){
-            alert('You cant buy a lot twice!');
-            selectedId='';
-            oneSelected=0;
-        });
-        document.querySelector('.btn-rej').addEventListener('click',function(){
-            document.getElementById(selectedId).style.backgroundColor='rgb(66, 134, 49)';
-            oneSelected=0;
-            selectedId='';
+            if (parkingLots[selectedId-1]==0){
+                parkingLots[selectedId-1]=0;
+                document.getElementById(selectedId).style.backgroundColor='rgb(66, 134, 49)';
+                oneSelected=0;
+                selectedId='';
+            } else {
+                parkingLots[selectedId-1]=0;
+                document.getElementById(selectedId).style.backgroundColor='rgb(66, 134, 49)';
+                oneSelected=0;
+                selectedId='';
+                numberOfLots++;
+                document.getElementById('clicks').textContent=numberOfLots;
+            }
         });
     }
 }
